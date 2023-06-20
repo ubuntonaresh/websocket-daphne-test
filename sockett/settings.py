c0e5118b47f74a78e9d6a72a88b37166486fc2d9
Naresh Kumar <banvro07@gmail.com>
2023-06-20 22:36:38 +0530

    a

--- sockett/settings.py
+++ sockett/settings.py
@@ -34,14 +34,14 @@ ALLOWED_HOSTS = ['whosthere.co' , 'www.whosthere.co', '89.117.37.186']
 
 
 INSTALLED_APPS = [
-    # "daphne",
+    "daphne",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'channels',
+    # 'channels',
     'socketapis'
 ]
 
